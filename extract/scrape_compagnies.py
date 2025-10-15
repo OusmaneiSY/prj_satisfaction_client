0083e65f9ec7d4ca9f31919097ef8eb4ecd94a7e
arnaudguilloux <arnaudguilloux@hotmail.com>
2025-10-15 12:34:25 +0000

    changements cosmetiques

--- extract/scrape_compagnies.py
+++ extract/scrape_compagnies.py
@@ -15,7 +15,10 @@ def get_company_data_from_trustpilot(company_name):
         str: Text content of the HTML code
     """
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+               "AppleWebKit/537.36 (KHTML, like Gecko) "
+               "Chrome/137.0.0.0 Safari/537.36"}
+
     page_url = "https://www.trustpilot.com/review/{}".format(company_name)
     page_html = requests.get(page_url, headers=headers)
 
@@ -34,39 +37,44 @@ def parse_company_data(company_data_html):
     """
 
     soup = bs(company_data_html, "lxml")
-    
-    company_raw_data = soup.find("script",attrs={"id" : "__NEXT_DATA__"})
-   
+
+    company_raw_data = soup.find("script", attrs={"id": "__NEXT_DATA__"})
+
     company_json_data = json.loads(company_raw_data.text)
 
     company_details_data = company_json_data["props"]["pageProps"]["businessUnit"]
-       
+
     if len(company_details_data['categories']) == 1:
         category = company_details_data['categories'][0]["name"]
     else:
         for item in company_details_data['categories']:
-            if item['isPrimary'] == True:
+            if item['isPrimary'] is True:
                 category = item["name"]
-        
+
     company_dict_data = {
                         'id': company_details_data['id'],
-                        'displayName' : company_details_data['displayName'],
-                        'numberOfReviews' : company_details_data['numberOfReviews'],
-                        'trustScore' : company_details_data['trustScore'],
-                        'websiteUrl' : company_details_data['websiteUrl'],
-                        'stars' : company_details_data['stars'],
-                        'category' : category,
-                        'email' : company_details_data['contactInfo']['email'],
-                        'address' : company_details_data['contactInfo']['address'],
-                        'city' : company_details_data['contactInfo']['city'],
-                        'country' : company_details_data['contactInfo']['country'],
-                        'phone' : company_details_data['contactInfo']['phone'],
-                        'zipCode' : company_details_data['contactInfo']['zipCode'],
-                        'five-star-rating-percent' : soup.find("label",attrs={"data-star-rating" : "five"}).find_next("p",attrs={"data-rating-distribution-row-percentage-typography" : "true"}).text,
-                        'four-star-rating-percent' : soup.find("label",attrs={"data-star-rating" : "four"}).find_next("p",attrs={"data-rating-distribution-row-percentage-typography" : "true"}).text,
-                        'three-star-rating-percent' : soup.find("label",attrs={"data-star-rating" : "three"}).find_next("p",attrs={"data-rating-distribution-row-percentage-typography" : "true"}).text,
-                        'two-star-rating-percent' : soup.find("label",attrs={"data-star-rating" : "two"}).find_next("p",attrs={"data-rating-distribution-row-percentage-typography" : "true"}).text,
-                        'one-star-rating-percent' : soup.find("label",attrs={"data-star-rating" : "one"}).find_next("p",attrs={"data-rating-distribution-row-percentage-typography" : "true"}).text
+                        'displayName': company_details_data['displayName'],
+                        'numberOfReviews': company_details_data['numberOfReviews'],
+                        'trustScore': company_details_data['trustScore'],
+                        'websiteUrl': company_details_data['websiteUrl'],
+                        'stars': company_details_data['stars'],
+                        'category': category,
+                        'email': company_details_data['contactInfo']['email'],
+                        'address': company_details_data['contactInfo']['address'],
+                        'city': company_details_data['contactInfo']['city'],
+                        'country': company_details_data['contactInfo']['country'],
+                        'phone': company_details_data['contactInfo']['phone'],
+                        'zipCode': company_details_data['contactInfo']['zipCode'],
+                        'five-star-rating-percent': soup.find("label", attrs={"data-star-rating": "five"})
+                                                        .find_next("p", attrs={"data-rating-distribution-row-percentage-typography": "true"}).text,
+                        'four-star-rating-percent': soup.find("label", attrs={"data-star-rating": "four"})
+                                                        .find_next("p", attrs={"data-rating-distribution-row-percentage-typography": "true"}).text,
+                        'three-star-rating-percent': soup.find("label", attrs={"data-star-rating": "three"})
+                                                         .find_next("p", attrs={"data-rating-distribution-row-percentage-typography": "true"}).text,
+                        'two-star-rating-percent': soup.find("label", attrs={"data-star-rating": "two"})
+                                                       .find_next("p", attrs={"data-rating-distribution-row-percentage-typography": "true"}).text,
+                        'one-star-rating-percent': soup.find("label", attrs={"data-star-rating": "one"})
+                                                       .find_next("p", attrs={"data-rating-distribution-row-percentage-typography": "true"}).text
                         }
 
     return company_dict_data
@@ -88,7 +96,8 @@ def create_company_data_dataframe(company_list):
     # loop to get company information and store it in companies_df dataframe
     for company in company_list:
         # collect and parse information of the company
-        company_row = parse_company_data(get_company_data_from_trustpilot(company))
+        company_html = get_company_data_from_trustpilot(company)
+        company_row = parse_company_data(company_html)
         # store collected information as a new raw in companies_df dataframe
         temp_df = pd.concat([temp_df, pd.DataFrame([company_row])], ignore_index=False)
 
@@ -99,10 +108,11 @@ def create_company_data_dataframe(company_list):
 
 
 def main_scrape_companies():
-    # create the list of company names we want to get information from Trustpilot
-    companies = ["www.showroomprive.com","loaded.com","westernunion.com","justfly.com","www.facebook.com"]
+    # create the list of company names
+    companies = ["www.showroomprive.com", "loaded.com",
+                 "westernunion.com", "justfly.com", "www.facebook.com"]
 
-    # collect company information in a dataframe 
+    # collect company information in a dataframe
     companies_df = create_company_data_dataframe(companies)
 
     # write the content of companies_df to a csv file
@@ -110,13 +120,20 @@ def main_scrape_companies():
 
 
 def test_get_company_data_from_trustpilot():
-    output = str(get_company_data_from_trustpilot("www.showroomprive.com")).find("www.showroomprive.com")
+    company = "www.showroomprive.com"
+    html = get_company_data_from_trustpilot(company)
+    output = str(html).find(company)
+
     assert output != -1
 
 
 def test_parse_company_data():
-    output = parse_company_data(get_company_data_from_trustpilot("www.showroomprive.com"))["displayName"]
-    assert output == "Showroomprive"
+    company = "www.showroomprive.com"
+    html = get_company_data_from_trustpilot(company)
+    data = parse_company_data(html)
+    output = data["displayName"]
+
+    assert output == company
 
 
 if __name__ == "__main__":
