b97b919b4fef4271fdc346c78548058035ecf0a7
arnaudguilloux <arnaudguilloux@hotmail.com>
2025-10-15 08:32:48 +0000

    ajout de commentaires et fonction main dans scrape_compagnies.ps

--- extract/scrape_compagnies.py
+++ extract/scrape_compagnies.py
@@ -4,15 +4,33 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 
 def get_trustpilot_html(company_name):
-    
+    """
+    Get HTML code from Trustpilot website review page
+
+    Args:
+        company_name (str): Name of the company.
+
+    Returns:
+        str: Text content of the HTML code
+    """
+
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"}
     page_url = "https://www.trustpilot.com/review/{}".format(company_name)
     page_html = requests.get(page_url, headers=headers)
     return page_html.content
 
-def get_company_data(company_html):
+def get_company_data(company_html_str):
+    """
+    Parses HTML text to extract company information
 
-    soup = bs(company_html, "lxml")
+    Args:
+        company_html_str (str): String containing the HTML content
+
+    Returns:
+        dict: company information
+    """
+
+    soup = bs(company_html_str, "lxml")
     
     company_raw_data = soup.find("script",attrs={"id" : "__NEXT_DATA__"})
    
@@ -50,14 +68,25 @@ def get_company_data(company_html):
 
     return company_dict_data
 
-companies = ["www.showroomprive.com","loaded.com","westernunion.com","justfly.com","www.facebook.com"]
+def main_scrape_companies():
+    # create the list of company names we want to get information from Trustpilot
+    companies = ["www.showroomprive.com","loaded.com","westernunion.com","justfly.com","www.facebook.com"]
 
-companies_df = pd.DataFrame()
+    # create an empty dataframe to store gathered company information
+    companies_df = pd.DataFrame()
 
-for company in companies:
-    company_row = get_company_data(get_trustpilot_html(company))
-    companies_df = pd.concat([companies_df, pd.DataFrame([company_row])], ignore_index=False)
+    # loop to get company information and store it in companies_df dataframe
+    for company in companies:
+        company_row = get_company_data(get_trustpilot_html(company))
+        companies_df = pd.concat([companies_df, pd.DataFrame([company_row])], ignore_index=False)
 
-companies_df = companies_df.set_index('id')
+    # set the companies_df index to 'id' column
+    companies_df = companies_df.set_index('id')
 
-companies_df.to_csv("companies_information.csv")
+    # write the content of companies_df to a csv file
+    companies_df.to_csv("companies_information.csv")
+
+
+if __name__ == "__main__":
+    print("Récupération des données globales des entreprises...")
+    main_scrape_companies()
