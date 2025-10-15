ba32bb913c282f6ce52a7619c2bdddf9656c0c8e
arnaudguilloux <arnaudguilloux@hotmail.com>
2025-10-15 13:01:34 +0000

    modification tests unitaires scrape_compagnies.py

--- extract/scrape_compagnies.py
+++ extract/scrape_compagnies.py
@@ -131,9 +131,9 @@ def test_parse_company_data():
     company = "www.showroomprive.com"
     html = get_company_data_from_trustpilot(company)
     data = parse_company_data(html)
-    output = data["websiteUrl"]
+    output = data["displayName"]
 
-    assert output == company
+    assert output == "Showroomprive"
 
 
 if __name__ == "__main__":
