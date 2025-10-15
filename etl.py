fd37b950e89a62e00f193c1cde892757b95b3d0d
arnaudguilloux <arnaudguilloux@hotmail.com>
2025-10-15 14:41:07 +0000

    modification etl.py : import des fonctions de scrape_compagnies

--- etl.py
+++ etl.py
@@ -7,12 +7,20 @@ appelées ici lorsque le code sera prêt.
 """
 
 import time
-
+from extract.scrape_compagnies import *
 
 def main_etl():
     """Fonction principale du pipeline ETL."""
     # TODO: Appelez les fonctions ici (extraction, transformation, chargement)
+    company_list = ["www.showroomprive.com", "loaded.com", "westernunion.com", "justfly.com", "www.facebook.com"]
 
+    # collect company information in a dataframe
+    companies_df = create_company_data_dataframe(company_list)
+
+    # write the content of companies_df to a csv file
+    companies_df.to_csv("companies_information.csv")
+
+    print(companies_df.head())
     # Petit délai pour éviter la fermeture immédiate du conteneur Docker
     time.sleep(5)
 
