ff82511732fcedfe2c64d9d0174f64b1eda8b99b
arnaudguilloux <arnaudguilloux@hotmail.com>
2025-10-17 14:37:11 +0000

    modif mineures dans etl_elt.py

--- etl_elt/scripts/etl_elt.py
+++ etl_elt/scripts/etl_elt.py
@@ -9,19 +9,20 @@ appelées ici lorsque le code sera prêt.
 import time
 from scrape_company_metadatas import *
 
-def main_etl():
+def main_etl_elt():
 
+    # répertoire de sortie
     outputFolder = "/app/extracts/"
+
     """Fonction principale du pipeline ETL."""
     # TODO: Appelez les fonctions ici (extraction, transformation, chargement)
     company_list = ["www.showroomprive.com", "loaded.com", "westernunion.com", "justfly.com", "www.facebook.com"]
+
     # collect company metadatas in a dataframe
-    companies_df = create_company_data_dataframe(company_list)
+    companies_metadatas_df = create_company_data_dataframe(company_list)
 
     # write the content of companies_df to a csv file
-    #companies_df.to_csv("./extract/data/companies_information.csv")
-    #companies_df.to_csv("/app/extracts/companies_information.csv")
-    companies_df.to_csv(outputFolder + "companies_metadatas.csv")
+    companies_metadatas_df.to_csv(outputFolder + "companies_metadatas.csv")
 
     # Petit délai pour éviter la fermeture immédiate du conteneur Docker
     time.sleep(5)
@@ -29,4 +30,4 @@ def main_etl():
 
 if __name__ == "__main__":
     print("Démarrage du script ETL...")
-    main_etl()
+    main_etl_elt()
